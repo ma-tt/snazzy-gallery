92cb4268f6b2468407c246356bb47bd7d1e7bcc9
ma-tt <16967859+ma-tt@users.noreply.github.com>
2025-05-14 00:25:02 -0400

    x

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ foreach (new DirectoryIterator('.') as $file) {
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            background: #a6a6a6;
+            background: #ffff;
             color: #222;
             border-radius: 8px;
             box-shadow: 0 2px 16px rgba(0,0,0,0.18);
